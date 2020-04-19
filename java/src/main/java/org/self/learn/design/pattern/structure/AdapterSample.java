0e969be2df96714e66b30628940e2f8b3d8d0dfb
Jiahao.Li <BluesJhao.Li@gmail.com>
2020-04-20 03:47:06 +0800

    And structure pattern.

---
+++ java/src/main/java/org/self/learn/design/pattern/structure/AdapterSample.java
@@ -0,0 +1,68 @@
+package org.self.learn.design.pattern.structure;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * 适配器模式：
+ * <p>
+ * 在不改变原始对象与目标对象前提下，将原始对象转化成目标对象的中间对象简称适配器对象，
+ *
+ * @author Li Jiahao
+ * @date 2020-04-18.
+ */
+public class AdapterSample {
+
+    public static void main(String[] args) throws IOException {
+        USB2 usb2 = new USB2();
+        TypeC typeC = new TypeCAdapter(usb2);
+        Mac.charging(typeC);
+
+        //JDK中的例子, 将字节流转化为字符流，inputStreamReader就是适配器
+        InputStream input = Files.newInputStream(
+                Paths.get("java/src/main/java/org/self/learn/design/pattern/document"));
+        Reader reader = new InputStreamReader(input, StandardCharsets.UTF_8);
+        char[] chats = new char[1024];
+        while (reader.read(chats) > 0) {
+            System.out.println(new String(chats));
+        }
+    }
+
+    private static class USB2 {
+        String connect() {
+            return "usb battery charging...";
+        }
+    }
+
+    private static abstract class TypeC {
+        public String connect() {
+            return "typec battery charging...";
+        }
+    }
+
+    static class Mac {
+        static void charging(TypeC typeC){
+            System.out.println(typeC.connect());
+        }
+    }
+
+    private static class TypeCAdapter extends TypeC {
+
+        private USB2 usb2;
+
+        TypeCAdapter(USB2 usb2) {
+            this.usb2 = usb2;
+        }
+
+        @Override
+        public String connect() {
+            return usb2.connect();
+        }
+    }
+}
---
+++ java/src/main/java/org/self/learn/design/pattern/structure/DecoratorSample.java
@@ -0,0 +1,99 @@
+package org.self.learn.design.pattern.structure;
+
+/**
+ * 装饰器模式：
+ * <p>
+ * 对于target目标需要新增功能时，通过decorator将新增功能附加到target对象上.
+ * decorator必须继承或实现target目标类
+ * <p>
+ * JDK中的应用：
+ * <p>
+ * InputStream 作为字节输入流，核心功能是字节流读取
+ * BufferedInputStream GZIPInputStream 拥有一个共同的父类FilterInputStream作为顶层装饰器
+ * BufferedInputStream GZIPInputStream是扩展了缓冲和解压功能的装饰器
+ * <p>
+ * 通过在JDK中的应用，实际意义如下：
+ * <p>
+ * 数据源需要扩展附加功能时: 可以通过在一个抽象的装饰器上面去扩展就要附加功能的装饰器即可
+ * 同时数据源也可以扩展核心功能: 比如 FileInputStream, ByteInputStream... 均可以用装饰器装饰
+ * <p>
+ * 做到数据源的核心功能扩展与附加功能扩展分离，提升扩展性
+ * <p>
+ * 举一个例子: 在内文本容上附加<span><bold><u></u></bold></span>进行渲染
+ *
+ * @author Li Jiahao
+ * @date 2020-04-19.
+ */
+public class DecoratorSample {
+
+    public static void main(String[] args) {
+        TextNode textNode = new TextNode("Text content!!!");
+
+        //进行渲染
+        TextNode spanTextDecorator = new SpanTextDecorator(textNode);
+        System.out.println(spanTextDecorator.getContent());
+
+        TextNode boldTextDecorator = new BoldTextDecorator(spanTextDecorator);
+        System.out.println(boldTextDecorator.getContent());
+    }
+
+
+    private static class TextNode {
+        public String content;
+
+        public TextNode() {
+        }
+
+        public TextNode(String content) {
+            this.content = content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getContent() {
+            return content;
+        }
+    }
+
+    private abstract static class TextDecorator extends TextNode {
+        protected TextNode target;
+
+        public TextDecorator(TextNode target) {
+            this.target = target;
+            setTarget(target);
+        }
+
+        public abstract void setTarget(TextNode target);
+
+        @Override
+        public String getContent() {
+            return target.content;
+        }
+    }
+
+    private static class SpanTextDecorator extends TextDecorator {
+        public SpanTextDecorator(TextNode target) {
+            super(target);
+        }
+
+        @Override
+        public void setTarget(TextNode textNode) {
+            target.content = String.format("<span>%s</span>", textNode.getContent());
+        }
+
+    }
+
+    private static class BoldTextDecorator extends TextDecorator {
+        public BoldTextDecorator(TextNode target) {
+            super(target);
+        }
+
+        @Override
+        public void setTarget(TextNode textNode) {
+            target.content = String.format("<bold>%s</bold>", textNode.getContent());
+        }
+
+    }
+}
